9efd8672f920d401f1b6403083e3991a017bc302
matrixoom <571255945@qq.com>
2026-03-15 21:52:42 +0800

    Task2

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,1 +1,25 @@
-﻿"""Domain models and rules."""
+"""Domain models and rules."""
+
+from .external_data import (
+    ConfidenceLevel,
+    EventHorizon,
+    MacroIndicatorReading,
+    MarketIndexSnapshot,
+    NewsCategory,
+    NewsItem,
+    ResearchFinding,
+    SearchResultItem,
+    SourceReference,
+)
+
+__all__ = [
+    "ConfidenceLevel",
+    "EventHorizon",
+    "MacroIndicatorReading",
+    "MarketIndexSnapshot",
+    "NewsCategory",
+    "NewsItem",
+    "ResearchFinding",
+    "SearchResultItem",
+    "SourceReference",
+]
--- src/providers/__init__.py
+++ src/providers/__init__.py
@@ -1,1 +1,33 @@
-﻿"""External data and API providers."""
+"""External data and API providers."""
+
+from .contracts import (
+    MacroDataProvider,
+    MarketDataProvider,
+    NewsProvider,
+    ProviderAvailability,
+    ProviderStatus,
+    ResearchProvider,
+    SearchProvider,
+)
+from .strategy import (
+    CategoryStrategy,
+    FeasibilityLevel,
+    SourcePlan,
+    StrategyCategory,
+    build_provider_strategy,
+)
+
+__all__ = [
+    "CategoryStrategy",
+    "FeasibilityLevel",
+    "MacroDataProvider",
+    "MarketDataProvider",
+    "NewsProvider",
+    "ProviderAvailability",
+    "ProviderStatus",
+    "ResearchProvider",
+    "SearchProvider",
+    "SourcePlan",
+    "StrategyCategory",
+    "build_provider_strategy",
+]
